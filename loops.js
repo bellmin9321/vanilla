56af81420115867ec148cd219285b2ef22d8e033
bellmin <bellmin9321@gmail.com>
2022-01-04 00:25:10 +0900

    add for loop

---
+++ loops.js
@@ -0,0 +1,14 @@
+var name = "bellmin king";
+
+// 1. reverse 함수 사용
+console.log(name.split("").reverse());
+
+// 2. for loop
+let reverse = '';
+
+for (let i = 0; i < name.length; i++) {
+  reverse = name[i]
+}
+
+console.log(reverse);
+
