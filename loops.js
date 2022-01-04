2dcd22b8d5adb1bd2088bc326bf8f1154e3350fd
bellmin <bellmin9321@gmail.com>
2022-01-04 22:26:33 +0900

    function 이용해서 문자열 역으로 한 글자씩 출력

--- loops.js
+++ loops.js
@@ -1,14 +1,25 @@
-var name = "bellmin king";
+var name = "bellmin"
 
 // 1. reverse 함수 사용
 console.log(name.split("").reverse());
+
 
 // 2. for loop
 let reverse = '';
 
 for (let i = 0; i < name.length; i++) {
-  reverse = name[i]
+  reverse = name.split("")[i]
+}
+console.log('-------------------');
+console.log(reverse);
+console.log('-------------------');
+// 3. function 이용
+
+function rev(str) {
+  return str.split("").reverse().join("");
 }
 
-console.log(reverse);
-
+var bell = rev("bellmin")
+for (let i = 0; i < bell.length; i++) {
+  console.log(bell[i]);
+}
