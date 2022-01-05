d4dca37983fe767107081d90bd02500401464d43
bellmin <bellmin9321@gmail.com>
2022-01-05 16:48:22 +0900

    js part2 Exercise not using  function ver

--- exercise.js
+++ exercise.js
@@ -1,4 +1,4 @@
-function fizzbuzz(i) {
+
   for (let i = 1; i <= 15; i++) {
     if ( i % 3 == 0 && i % 5 == 0) {
       console.log('fizzbuzz');
@@ -10,5 +10,5 @@ function fizzbuzz(i) {
       console.log(i);
     }
   }
-}
-console.log(fizzbuzz());
+
+  
