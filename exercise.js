655c716d3ad547d00c8eeab8966397a68575e11f
bellmin <bellmin9321@gmail.com>
2022-01-05 17:46:00 +0900

    JS part1 exercise 2. Biggest Number

--- exercise.js
+++ exercise.js
@@ -1,14 +1,28 @@
+// 1. normal ver
+  // for (let i = 1; i <= 5; i++) {
+  //   if ( i % 3 == 0 && i % 5 == 0) {
+  //     console.log('fizzbuzz');
+  //   } else if ( i % 3 == 0 ) {
+  //     console.log('fizz');
+  //   } else if ( i % 5 == 0) {
+  //     console.log('buzz');
+  //   } else {
+  //     console.log(i);
+  //   }
+  // }
 
-  for (let i = 1; i <= 15; i++) {
-    if ( i % 3 == 0 && i % 5 == 0) {
-      console.log('fizzbuzz');
-    } else if ( i % 3 == 0 ) {
-      console.log('fizz');
-    } else if ( i % 5 == 0) {
-      console.log('buzz');
-    } else {
-      console.log(i);
-    }
+  // 2. Biggest Number
+  var a = 10;
+  var b = 20;
+  var c = 30;
+// a>b>c a>c>b / b>a>c b>c>a / c>a>b c>b>a
+function maxNum() {
+  if( a > b && a > c) { 
+    console.log(a);
+  } else if (b > a && b > c) {
+    console.log(b);
+  } else {
+    console.log(c);
   }
-
-  
+}
+console.log(maxNum());
