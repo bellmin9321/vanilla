2af5eda4b7d57c3199fcbc6625bcc0493568ede2
bellmin <bellmin9321@gmail.com>
2022-01-06 15:45:17 +0900

    exercise 3. word position 2번째 풀이

--- exercise.js
+++ exercise.js
@@ -29,9 +29,20 @@
   var word = "dolphin";
   var sentence = "Where did Jane get the dolphin from, John?";
   
+  console.log(sentence.split(''));
+console.log(sentence.split('').lastIndexOf('n',-3));
+  
 var indexOfD = sentence.indexOf(word);
 console.log();
+// 1. 첫번째 방법
 var indexOfN = sentence.split('').lastIndexOf(word[word.length-1], -3);
+
+console.log(indexOfD, indexOfN);
+
+// 2. 2번째 방법
+var indexOfD = sentence.indexOf(word);
+console.log(word.length-1);
+var indexOfN = indexOfD + word.length - 1
 
 console.log(indexOfD, indexOfN);
 
