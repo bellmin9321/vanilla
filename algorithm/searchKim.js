a51f25eed60d0b634288932e25954d848ef93d4a
bellmin <bellmin9321@gmail.com>
2022-01-14 22:52:12 +0900

    김서방 찾기 start

---
+++ algorithm/searchKim.js
@@ -0,0 +1,13 @@
+var seoul = ['Kim', 'Jane', 'Lee', 'Joo', 'Ki'];
+
+function solution(seoul) {
+  for (let i = 0; 1 <= seoul.length <= 1000; i++) {
+    if ( 1 <= seoul[i].length <= 20) {
+      
+    }
+  }
+  var answer = "김서방은" + seoul[i] +"에 있다";
+  return answer;
+};
+
+console.log(answer);
